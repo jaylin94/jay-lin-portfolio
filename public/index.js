98bea15f444f30e8a5f7b88b01d31df20380a800
jaylin94 <jaylin94@gmail.com>
2019-07-18 15:07:53 -0400

    Change header to ID, add logic to remove top padding on mobile devices

--- public/index.js
+++ public/index.js
@@ -1,2 +1,11 @@
 var projectCards = document.querySelectorAll(".card");
 var projectTop = document.getElementById("projectTop")
+var header = document.getElementById("header");
+
+
+if (header.offsetWidth > 512) {
+  header.style.paddingTop = "20vh";
+}
+else {
+  header.style.paddingTop = "5vh";
+}
