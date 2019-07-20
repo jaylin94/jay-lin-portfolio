816c399c21a90b5a8b9732bc031ad67902c1584b
jaylin94 <jaylin94@gmail.com>
2019-07-19 22:45:36 -0400

    Refactor scaling font sizes, adjust styling for project descriptions

--- public/index.js
+++ public/index.js
@@ -1,11 +1,3 @@
 var projectCards = document.querySelectorAll(".card");
 var projectTop = document.getElementById("projectTop")
 var header = document.getElementById("header");
-
-
-if (header.offsetWidth > 512) {
-  header.style.paddingTop = "20vh";
-}
-else {
-  header.style.paddingTop = "5vh";
-}
