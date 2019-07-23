7029910b228e80a103521fe2b6fd38070d8ef4bc
jaylin94 <jaylin94@gmail.com>
2019-07-22 22:30:59 -0400

    Update contact form to utilize formspree

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
 var projectCards = document.querySelectorAll(".card");
 var projectTop = document.getElementById("projectTop")
 var header = document.getElementById("header");
+var mailBtn = document.getElementById("mailBtn");
