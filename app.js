13e711d9e187a85cd7d4f9793715bff626ff1384
jaylin94 <jaylin94@gmail.com>
2019-07-26 14:27:18 -0400

    Fix PORT number for hosting

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const request = require("request");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(9392, process.env.IP);
+app.listen(process.env.PORT, process.env.IP);
